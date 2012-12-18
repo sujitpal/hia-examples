7729344bcd80060a35ec1ace1eb54d4eb3f0bfc0
Matteus Klich <matteus.klich@gmail.com>
2012-12-17 16:57:34 -0800

    Added main method to all Parts, so that each part can be easily executed in an IDE

--- scala/scalding-impatient/src/main/scala/com/mycompany/impatient/Part1.scala
+++ scala/scalding-impatient/src/main/scala/com/mycompany/impatient/Part1.scala
@@ -2,6 +2,16 @@ package com.mycompany.impatient
 
 import com.twitter.scalding._
 
+object Part1 {
+  def main(args: Array[String]) {
+    (new Part1(Args(List("--local", "", "--input", "data/rain.txt", "--output", "data/output.txt")))).run
+
+    import io.Source
+    for (line <- Source.fromFile("data/output.txt").getLines())
+      println(line)
+  }
+}
+
 /**
  * Scalding version of "Cascading for the Impatient, Part I"
  * Article: http://www.cascading.org/2012/07/02/cascading-for-the-impatient-part-1/
