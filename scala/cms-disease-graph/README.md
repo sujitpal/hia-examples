b6e3a4ba2882f1930427fc36c0e396c68b33b2d6
Sujit Pal <sujit.pal@comcast.net>
2014-12-07 10:24:26 -0800

    Initial revision for cms-disease-graph project (based on Snowplow Analytic's spark-example-project project, updated to work with Scala 2.10), and adding in cms-preproc project from earlier.
    Former-commit-id: 51c31214c8a0b1babf2028d4212a3df4cb9a5dec

---
+++ scala/cms-disease-graph/README.md
@@ -0,0 +1,4 @@
+# CMS Disease Graph Generator
+
+Processes Claims data from Centers for Medicare and Medicaid Services (cms.gov) to create a graph of related diseases, based on the overlap of procedure codes across claims from members with known comorbidities.
+
---
+++ scala/cms-disease-graph/project/Dependencies.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2013 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+import sbt._
+
+object Dependencies {
+  val resolutionRepos = Seq(
+    "Akka Repository" at "http://repo.akka.io/releases/",
+    "Spray Repository" at "http://repo.spray.cc/"
+  )
+
+  object V {
+    val spark     = "1.1.0"
+    val specs2    = "1.13"
+    val guava     = "11.0.1"
+    // Add versions for your additional libraries here...
+  }
+
+  object Libraries {
+    val sparkCore    = "org.apache.spark"           %% "spark-core"            % V.spark        % "provided"
+    // Add additional libraries from mvnrepository.com (SBT syntax) here...
+
+    // Scala (test only)
+    val specs2       = "org.specs2"                 % "specs2_2.10"           % V.specs2       % "test"
+    val guava        = "com.google.guava"           % "guava"                  % V.guava        % "test"
+  }
+}
